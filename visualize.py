3c2fc139dfe528823003df239577a10a583ba531
RBC <jordan.richards@rbc.com>
2019-03-27 18:21:32 -0400

    Added highlight command and the ability to serve multiple connections at once.

--- visualize.py
+++ visualize.py
@@ -9,9 +9,10 @@ class RDD:
     def __init__(self, id, deps = []):
         self.id = id
         self.deps = deps
+        self.highlighted = False
 
     def __str__(self):
-        return 'RDD %d' % self.id
+        return '%d' % self.id
 
     def set_deps(self, deps):
         self.deps = deps
@@ -23,71 +24,84 @@ class RDD:
             if not to_add in g.edges():
                 g.add_edge(*to_add)
 
-def server_loop(sock, rdds, rdds_lock):
+def server_loop(sock, rdds, edit_lock):
     with sock:
         with sock.makefile('r') as f:
             for line in f:
-                with rdds_lock:
-                    if line.strip() == 'clear':
-                        rdds.clear()
-                        rdds['clear'] = True
-                        continue
-
+                with edit_lock:
                     words = line.split()
 
-                    id = int(words[0])
-                    deps = []
+                    if words[0].strip() == 'clear':
+                        rdds.clear()
+                        print('clear')
+                        rdds['clear'] = True
+                    elif words[0].strip() == 'highlight':
+                        n = int(words[1])
 
-                    if not id in rdds:
-                        rdds[id] = RDD(id)
-                        print('created',rdds[id])
+                        to_highlight = [int(words[2 + i]) for i in range(n)]
+                        for id in rdds:
+                            if type(id) is int:
+                                rdds[id].highlighted = id in to_highlight
+                    else:
+                        id = int(words[0])
+                        deps = []
 
-                    ndeps = int(words[1])
-                    for i in range(ndeps):
-                        oid = int(words[2 + i])
+                        if not id in rdds:
+                            rdds[id] = RDD(id)
+                            print('created',rdds[id])
 
-                        if not oid in rdds:
-                            rdds[oid] = RDD(oid)
-                            print('created',rdds[oid])
+                        ndeps = int(words[1])
+                        for i in range(ndeps):
+                            oid = int(words[2 + i])
 
-                        print(rdds[id],'->',rdds[oid])
+                            if not oid in rdds:
+                                rdds[oid] = RDD(oid)
+                                print('created',rdds[oid])
 
-                        deps += [rdds[oid]]
+                            print(rdds[id],'->',rdds[oid])
 
-                    rdds[id].set_deps(deps)
+                            deps += [rdds[oid]]
 
-def run_server(port, rdds, rdds_lock):
+                        rdds[id].set_deps(deps)
+
+def run_server(port, rdds, edit_lock):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind((socket.gethostname(), port))
     serversocket.listen(1)
 
-    conn, addr = serversocket.accept()
+    while True:
+        conn, addr = serversocket.accept()
 
-    print('Connected at',addr)
+        print('Connected at',addr)
 
-    thread = Thread(target=server_loop, args=(conn, rdds, rdds_lock))
-    thread.start()
-    return thread
+        Thread(target=server_loop, args=(conn, rdds, edit_lock)).start()
 
-# wait for a connection
+
 rdds = {}
-rdds_lock = Lock()
-server = run_server(8001, rdds, rdds_lock)
+edit_lock = Lock()
+server_thread = Thread(target=run_server, args=(8001, rdds, edit_lock))
+server_thread.start()
 
 plt.ion()
 plt.show()
 
+color_map = []
 g = nx.DiGraph()
-while server.is_alive():
-    with rdds_lock:
+while server_thread.is_alive():
+    with edit_lock:
         if 'clear' in rdds:
             g.clear()
             del rdds['clear']
 
         plt.clf()
+        color_map = []
         for id in rdds:
             rdds[id].draw(g)
-        pos = nx.drawing.nx_agraph.graphviz_layout(g, prog='dot')
-        nx.draw(g, pos, with_labels=True, node_size=1000)
-        plt.draw()
-        plt.pause(2)
+
+        for node in g.nodes():
+            color_map += ['blue' if node.highlighted else 'red']
+
+    pos = nx.drawing.nx_agraph.graphviz_layout(g, prog='dot')
+    nx.draw(g, pos, node_color = color_map, with_labels=True, node_size=1000)
+    plt.draw()
+    plt.pause(2)
